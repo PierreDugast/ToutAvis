9d207e91e177e29566a2825db8b49e99f3f999cc
pierre.dugast <pierredugast1@gmail.com>
2019-04-30 19:28:18 +0200

    Ajout Readme.txt

---
+++ src/tests/AddItemBookTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import opinion.ISocialNetwork;
+import opinion.SocialNetwork;
+
+
+import exceptions.BadEntryException;
+import exceptions.MemberAlreadyExistsException;
+import exceptions.NotTestReportException;
+
+public class AddIte
