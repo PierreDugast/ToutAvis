768d1f46e7434cfe204bd77a1c908e3e6453ba5a
PierreDugast <50150298+PierreDugast@users.noreply.github.com>
2019-05-01 23:38:34 +0200

    Amélioration reviewItemBook

--- src/tests/ReviewItemBookTest.java
+++ src/tests/ReviewItemBookTest.java
@@ -6,38 +6,43 @@ import exceptions.NotTestReportException;
 import exceptions.NotItemException;
 import opinion.ISocialNetwork;
 import opinion.SocialNetwork;
-
+/* 
+ * Tests for opinion.SocialNetwork.reviewItemBook method
+ * @author Pierre Dugast
+ * Date 01/05/2019
+ * */
 
 public class ReviewItemBookTest{
 	
 	private static int reviewItemBookBadEntryTest(ISocialNetwork sn, String login,
 			String password, String title, float mark,String comment, String testId, String errorMessage) {
 
-	
 		int nbBooks = sn.nbBooks();		
+		
 		try {
 			sn.reviewItemBook(login, password, title, mark, comment); 
 			System.out.println("Err " + testId + " : " + errorMessage); 
 			return 1; 
-		} catch (BadEntryException e) { 
+		} 
+		
+		catch (BadEntryException e) { 
 			if (sn.nbBooks() != nbBooks) { 
 				System.out
-						.println("Err "
-								+ testId
-								+ " : BadEntry was thrown but the number of books was changed"); 
+						.println("Err "+ testId+ " : BadEntry was thrown but the number of books was changed"); 
 				return 1;
-			} else
-				
-				return 0; 
-		} catch (Exception e) { 
-			System.out.println("Err " + testId + " : unexpected exception. "
-					+ e); 
+			} 
+			else return 0; 
+			
+		} 
+		
+		catch (Exception e) { 
+			System.out.println("Err " + testId + " : unexpected exception. "+ e); 
 			e.printStackTrace();
 			return 1; 
 		}
 	}
 
-	private static int rewiewItemBookNotMemberTest(ISocialNetwork sn, String login,
+	private static int reviewItemBookNotMemberTest(ISocialNetwork sn, String login,
 			String password, String title, float mark,String comment, String testId, String errorMessage) {
 
 		int nbBooks = sn.nbBooks();	
@@ -45,18 +50,18 @@ public class ReviewItemBookTest{
 			sn.reviewItemBook(login, password, title, mark, comment);
 			System.out.println("Err " + testId + " : " + errorMessage); 
 			return 1; 
-		} catch (NotMemberException e) {
+		} 
+		
+		catch (NotMemberException e) {
 			if (sn.nbBooks() != nbBooks) {
-				System.out
-						.println("Err "
-								+ testId
-								+ " : reviewItemBooksNotMember was thrown, but the number of books was changed"); 
+				System.out.println("Err "+ testId+ " : reviewItemBooksNotMember was thrown, but the number of books was changed"); 
 				return 1;
-			} else
-				return 0; 
-		} catch (Exception e) { 
-			System.out.println("Err " + testId + " : unexpected exception. "
-					+ e); 
+			} 
+			else return 0; 
+		} 
+		
+		catch (Exception e) { 
+			System.out.println("Err " + testId + " : unexpected exception. "+ e); 
 			e.printStackTrace();
 			return 1; 
 		}
@@ -64,20 +69,21 @@ public class ReviewItemBookTest{
 
 	private static int reviewItemBookNotItemTest(ISocialNetwork sn, String login,
 			String password, String title, float mark,String comment, String testId, String errorMessage) {
+		
 		int nbBooks = sn.nbBooks(); 
+		
 		try {
 			sn.reviewItemBook(login, password, title, mark, comment);
 			System.out.println("Err " + testId + " : " + errorMessage);
 			if (sn.nbBooks() != nbBooks) { 
-				System.out.println("Err " + testId
-						+ " : the number of books (" + nbBooks
-						+ ") was not incremented"); 
+				System.out.println("Err " + testId+ " : the number of books (" + nbBooks+ ") was not incremented"); 
 				return 1; 
-			} else
-				return 0; 
-		} catch (Exception e) {
-			System.out
-					.println("Err " + testId + " : unexpected exception " + e); 
+			} 
+			else return 0; 
+		} 
+		
+		catch (Exception e) {
+			System.out.println("Err " + testId + " : unexpected exception " + e); 
 			e.printStackTrace(); 
 			return 1;
 		}
@@ -91,15 +97,15 @@ public class ReviewItemBookTest{
 		try {
 			sn.reviewItemBook(login, password, title, mark, comment);
 			if (sn.nbBooks() != nbBooks + 1) { 
-				System.out.println("Err " + testId
-						+ " : the number of books (" + nbBooks
-						+ ") was not incremented"); 
+				System.out.println("Err " + testId+ " : the number of books (" + nbBooks+ ") was not incremented"); 
 				return 1; 
-			} else
-				return 0; 
-		} catch (Exception e) {
-			System.out
-					.println("Err " + testId + " : unexpected exception " + e); 
+			} 
+			
+			else return 0; 
+		} 
+
+		catch (Exception e) {
+			System.out.println("Err " + testId + " : unexpected exception " + e); 
 			e.printStackTrace(); 
 			return 1;
 		}
@@ -109,83 +115,82 @@ public class ReviewItemBookTest{
 	public static TestReport test(){
 
 		ISocialNetwork sn = new SocialNetwork();
-
 		int nbBooks = sn.nbBooks(); // number of books in 'sn' (should be 0
-									// here)
+		// here)
 		int nbFilms = sn.nbFilms(); // number of films in 'sn' (should be 0
-									// here)
-
+//here)
 		int nbTests = 0; // total number of performed tests
 		int nbErrors = 0; // total number of failed tests
 
 		System.out.println("Testing reviewItemBook()");
 
-		// <=> test nÂ°1
+		// <=> test n°1
 
 		// check if incorrect parameters cause reviewItemBook() to throw BadEntry
 		// exception
 
 		nbTests++;
-		nbErrors += reviewItemBookBadEntryTest(sn, null, "pass", "titre",3,"comment", "4.1",
+		nbErrors += reviewItemBookBadEntryTest(sn, null, "password", "un titre",3,"un commentaire", "4.1",
 				"reviewItemBooks() doesn't reject null logins");
 	
 		nbTests++;
-		nbErrors += reviewItemBookBadEntryTest(
-				sn,
-				" ",
-				"pass", "titre",3,"comment", "4.2",
+		nbErrors += reviewItemBookBadEntryTest(sn," ","password", "un titre",3,"un commentaire", "4.2",
 				"reviewItemBooks() doesn't reject logins that don't contain at least one character other than space");
+		
 		nbTests++;
-		nbErrors += reviewItemBookBadEntryTest(sn, "B", null,"titre",3,"comment", "4.3",
+		nbErrors += reviewItemBookBadEntryTest(sn, "login", null,"un titre",3,"un commentaire", "4.3",
 				"reviewItemBooks() doesn't reject null passwords");
+		
 		nbTests++;
-		nbErrors += reviewItemBookBadEntryTest(
-				sn,
-				"B",
-				"   qwd ","titre",3,"comment", "4.4",
+		nbErrors += reviewItemBookBadEntryTest(sn,"login","   qwd ","un titre",3,"un commentaire", "4.4",
 				"reviewItemBooks() doesn't reject passwords that don't contain at least 4 characters (not taking into account leading or trailing blanks)");
 		
 		nbTests++;
-		nbErrors += reviewItemBookBadEntryTest(sn, "BBBB", "bbbb", null,3,"comment", "4.5",
+		nbErrors += reviewItemBookBadEntryTest(sn, "login", "password", null,3,"un commentaire", "4.5",
 				"reviewItemBooks() doesn't reject null titles");
 		
 		nbTests++;
-		nbErrors += reviewItemBookBadEntryTest(sn, "BBBB", "bbbb", " ",3,"comment", "4.6",
+		nbErrors += reviewItemBookBadEntryTest(sn, "login", "password", " ",3,"un commentaire", "4.6",
 				"reviewItemBooks() doesn't reject titles that don't contain at least one character other than space");
 		
 		nbTests++;
-		nbErrors += reviewItemBookBadEntryTest(sn, "BBBB", "bbbb", "titre",-1,"comment", "4.7",
-				"reviewItemBooks() doesn't reject marks that isn't greater or equals to 0");
+		nbErrors += reviewItemBookBadEntryTest(sn, "login", "password", "un titre",-1,"un commentaire", "4.7",
+				"reviewItemBooks() doesn't reject marks that aren't greater or equals to 0");
 		
 		nbTests++;
-		nbErrors += reviewItemBookBadEntryTest(sn, "BBBB", "bbbb", "titre",6,"comment", "4.8",
-				"reviewItemBooks() doesn't reject marks that isn't lesser or equals to 5");
+		nbErrors += reviewItemBookBadEntryTest(sn, "login", "password", "un titre",6,"un commentaire", "4.8",
+				"reviewItemBooks() doesn't reject marks that aren't lesser or equals to 5");
 		
 		nbTests++;
-		nbErrors += reviewItemBookBadEntryTest(sn, "BBBB", "bbbb", "titre",4,null, "4.9",
+		nbErrors += reviewItemBookBadEntryTest(sn, "login", "password", "un titre",4,null, "4.9",
 				"reviewItemBooks() doesn't reject null comments");
 
-		// <=> test nÂ°2
+		// <=> test n°2
 		//populate with 1 member
-
+/*
 		nbTests++;
 		nbErrors += reviewItemBookOKTest(sn, "BBBB", "bbbb","titre",3,"comment", "5.0");
-
+		*/
+		try{sn.addMember("A new member", "a password", "test profile");}
+		catch(Exception e) {System.out.println("User not created!!");};
 		
 		nbTests++;
-		nbErrors += rewiewItemBookNotMemberTest(sn, new String("AAAA"), "bbbb","titre",3,"comment", "5.1",
-				"The login of the first member was accepted as login for a new book");
+		nbErrors += reviewItemBookNotMemberTest(sn, "Unregistered", "bbbb","titre",3,"comment", "5.1",
+				"reviewItemBook does not reject unregistered members");
 		
 		nbTests++;
-		nbErrors += rewiewItemBookNotMemberTest(sn, "BBBB", new String("aaaa"),"titre",3,"comment", "5.2",
-				"The password of the first member was accepted as password for a new book");
+		nbErrors += reviewItemBookNotMemberTest(sn, "A new member", "wrong password","titre",3,"comment", "5.2",
+				"reviewItemBook does not reject member with wrong password");
 		
-		// <=> test nÂ°3
 		
+		
+		// <=> test n°3
+		nbTests++;
+		nbErrors += reviewItemBookOKTest(sn, "A new member", "a password","un titre",3,"un commentaire", "6.0");
 		
 		nbTests++;
-		nbErrors += reviewItemBookNotItemTest(sn, "BBBB", "bbbb",new String("rouge"),3,"comment", "6.1",
-				"The title of the first book was accepted as title for a new book");
+		nbErrors += reviewItemBookNotItemTest(sn, "A new member", "a password","un autre titre",3,"un commentaire", "6.1",
+				"reviewItemBook does not reject items with wrong title");
 
 
 		
